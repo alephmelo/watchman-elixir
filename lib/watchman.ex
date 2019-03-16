566e729a6e878bb29f263cf2479f4b492d618e31
Aleph Melo <alephmelo@icloud.com>
2019-03-16 23:53:47 +0100

    Remove hello world test.

--- lib/watchman.ex
+++ lib/watchman.ex
@@ -3,19 +3,6 @@ defmodule Watchman do
   Documentation for Watchman.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Watchman.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def main(dir \\ ".") do
     Enum.each(
       Utils.list_subdirs(dir),
--- test/watchman_test.exs
+++ test/watchman_test.exs
@@ -1,8 +1,4 @@
 defmodule WatchmanTest do
   use ExUnit.Case
   doctest Watchman
-
-  test "greets the world" do
-    assert Watchman.hello() == :world
-  end
 end

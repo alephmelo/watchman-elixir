16aebe4708e4d50a26c66306224979865b7189ac
Aleph Melo <alephmelo@icloud.com>
2019-03-16 16:25:37 +0100

    Add path join for relative path check.

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,7 +1,7 @@
 defmodule Utils do
   def list_subdirs(dir \\ ".") do
     Enum.filter(File.ls!(dir), fn sub_dir ->
-      if File.dir?(sub_dir) and not String.starts_with?(sub_dir, ".") do
+      if File.dir?(Path.join(dir, sub_dir)) and not String.starts_with?(sub_dir, ".") do
         sub_dir
       end
     end)
